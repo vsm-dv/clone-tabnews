8afce59017870b113f5e3ff9daa4d3618b048cfd
vsm-dv <mt.valeria1@gmail.com>
2025-02-08 17:54:37 -0300

    feat: finish api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,35 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query({ text: "SELECT 1 + 1 as sum" });
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ message: "alunos do curso.dev são pessoas acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersion = (
+    await database.query({ text: "SHOW server_version;" })
+  )?.rows[0].server_version;
+
+  const max_connections = (
+    await database.query({ text: "SHOW max_connections;" })
+  )?.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const open_connections = (
+    await database.query({
+      text: `SELECT count(*)::INT FROM pg_stat_activity WHERE datname = $1;`,
+      values: [databaseName],
+    })
+  )?.rows[0].count;
+
+  return response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: Number(max_connections),
+        open_connections,
+      },
+    },
+  });
 }
 
 export default status;
