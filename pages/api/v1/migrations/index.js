3a6929734a657d73250de7928358e4a4a9f4e517
vsm-dv <mt.valeria1@gmail.com>
2025-02-13 23:02:40 -0300

    fix: add end to response status

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -37,5 +37,5 @@ export default async function migrations(request, response) {
     return response.status(200).json(migratedMigrations);
   }
 
-  return response.status(405);
+  return response.status(405).end();
 }
