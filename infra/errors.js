e3e739cca67f2776c9fa617f0c58f638ea0bd000
vsm-dv <mt.valeria1@gmail.com>
2025-03-22 22:30:20 -0300

    feat: create NotFoundError to use it in controller

--- infra/errors.js
+++ infra/errors.js
@@ -55,6 +55,25 @@ export class ValidationError extends Error {
   }
 }
 
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "The resource was not found in the system.", { cause });
+
+    this.name = "NotFoundError";
+    this.action = action || "Check if the sent data is correct";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
 export class MethodNotAllowedError extends Error {
   constructor() {
     super("Method not allowed on this endpoint");
